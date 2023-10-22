c7af320b631cced64b8e84587f5e0b8a1dee3132
fadli131 <fadliaryadinata011@gmail.com>
2023-10-22 18:56:18 +0700

    first

--- src/pages/function/index.js
+++ src/pages/function/index.js
@@ -1,6 +1,6 @@
 import Swal from "sweetalert2"
 
-export const API_BASE_URL = 'https://us-central1-revou-fullstack-2.cloudfunctions.net/fadliweek17'
+export const API_BASE_URL = 'https://us-central1-revou-fullstack-2.cloudfunctions.net/fadliweek_17'
 
 export const getHeaders = () => {
     const token = localStorage.getItem('token');
